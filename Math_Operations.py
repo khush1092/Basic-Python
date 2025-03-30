6e4293f96127652854a49a568591aece8d1923b1
Khush Shah <khush82004@gmail.com>
2025-03-30 19:14:55 +0530

    Add files via upload

---
+++ Math_Operations.py
@@ -0,0 +1,9 @@
+#Maths Calculation
+A = int(input("Enter Value for A"))
+B = int(input("Enter Value for B"))
+
+a1 = A + B
+a2 = A - B
+a3 = A * B
+a4 = A / B
+print( "Addition:",a1,'Subtraction:',a2,'Multiply:',a3,'Divide:',a4)
---
+++ Name.py
@@ -0,0 +1,5 @@
+# Concate Full name
+First_name = input("Enter First Name")
+Last_name = input("Enter Last Name")
+Full_name = First_name + Last_name
+print('Hello, ',Full_name ,'Good to See U')
